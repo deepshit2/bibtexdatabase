953fe18cd502ec51f867b765412a19d90ffd6030
Jukka-Pekka Seppänen <jukka-pekka.seppanen@helsinki.fi>
2015-09-28 13:35:46 +0300

    Book controller

--- bibtexdatabase/src/main/java/wad/controller/BookController.java
+++ bibtexdatabase/src/main/java/wad/controller/BookController.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package wad.controller;
 
-/**
- *
- * @author Jukkis
- */
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import wad.domain.Book;
+import wad.repository.BookRepository;
+
+@Controller
+@RequestMapping(value="/books")
 public class BookController {
+    
+    @Autowired
+    BookRepository bookRepository;
+    
+    @RequestMapping(method = RequestMethod.POST)
+    public String createBook(@ModelAttribute("book") Book book){
+        bookRepository.save(book);
+        return "redirect:/books";
+    }
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Book> getBooks(){
+        return bookRepository.findAll();
+    }
     
 }
