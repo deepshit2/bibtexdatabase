b7c33f304a0fd05fb8aa40cf2c60168d0f3f2eb6
Jukka-Pekka Seppänen <jukka-pekka.seppanen@helsinki.fi>
2015-09-28 13:48:33 +0300

    Book entity

--- bibtexdatabase/src/main/java/wad/controller/BookController.java
+++ bibtexdatabase/src/main/java/wad/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import wad.domain.Book;
@@ -27,4 +28,9 @@ public class BookController {
         return bookRepository.findAll();
     }
     
+    @RequestMapping(value="/{id}", method = RequestMethod.DELETE)
+    public String deleteBook(@PathVariable Long id){
+        bookRepository.delete(id);
+        return "redirect:/books";
+    }
 }
--- bibtexdatabase/src/main/java/wad/domain/Book.java
+++ bibtexdatabase/src/main/java/wad/domain/Book.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.domain.AbstractPersistable;
 @Entity
 public class Book extends AbstractPersistable<Long> {
     
+    private String author, title, publisher, address, isbn, note;
+    private Integer year, volume, edition, month, series;
+    
+    
 }
