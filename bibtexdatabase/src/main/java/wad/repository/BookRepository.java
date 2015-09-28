ce6c47f279ee0698fa134bb21b04578c2b1c92f9
Jukka-Pekka Seppänen <jukka-pekka.seppanen@helsinki.fi>
2015-09-28 13:27:15 +0300

    Book controller

---
+++ bibtexdatabase/src/main/java/wad/repository/BookRepository.java
@@ -0,0 +1,9 @@
+
+package wad.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import wad.domain.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+    
+}
