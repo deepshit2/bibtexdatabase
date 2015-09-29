4bab8deb5284a556280a5275d2f9d602c4d31dc0
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2015-09-29 20:33:04 +0300

    osa testeistä näin alkuun

---
+++ bibtexdatabase/src/test/java/NewArticleTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.fluentlenium.adapter.FluentTest;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ *
+ * @author santeri
+ */
+public class NewArticleTest extends FluentTest {
+    
+public WebDriver webDriver = new HtmlUnitDriver();
+    public WebDriver getDefaultDriver(){
+        return webDriver;
+    }
+    
+    @Test
+    public void submitArticle(){
+        goTo("http://bibtexdatabase.herokuapp.com/articles/new");
+        fill("#author").with("Santeri");
+        fill("#title").with("Eeppinen kandi");
+        submit("input[type=submit]");
+        assertTrue(pageSource().contains("New article created"));
+    }
+}
