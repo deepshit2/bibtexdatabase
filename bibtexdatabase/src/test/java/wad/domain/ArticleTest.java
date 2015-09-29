f0d8fcf650db83bf0035f5afdcf707d64e60a7f4
ued1 <ued1@users.noreply.github.com>
2015-09-29 21:55:46 +0300

    ArticleTest

---
+++ bibtexdatabase/src/test/java/wad/domain/ArticleTest.java
@@ -0,0 +1,39 @@
+
+package wad.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+
+public class ArticleTest {
+    
+    @Test
+    public void createArticle() {
+        Article article = new Article();
+
+        //Setterit
+        article.setAuthor("Author");
+        article.setJournal("jossain");
+        article.setNumber(3);
+        article.setPages(666);
+        article.setMonth(1);
+        article.setNote("joooo");
+        article.setTitle("Title");
+        article.setVolume(1);
+        article.setYear(2015);
+
+        //Getterit
+        assertEquals("Author", article.getAuthor());
+        assertEquals("jossain", article.getJournal());
+        assertTrue(3 == article.getNumber());
+        assertTrue(666 == article.getPages());
+        assertTrue(1 == article.getMonth());
+        assertEquals("joooo", article.getNote());
+        assertEquals("Title", article.getTitle());
+        assertTrue(1 == article.getVolume());
+        assertTrue(2015 == article.getYear());
+        
+    }
+    
+}
