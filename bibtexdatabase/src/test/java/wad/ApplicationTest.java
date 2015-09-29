1753ac352d4fad146abc26e4303c2d329e9a9287
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2015-09-29 22:46:50 +0300

    Kokeilin vielä testiluokan uudelleen nimeämistä ja siirtoa

--- bibtexdatabase/src/test/java/wad/ApplicationTest.java
+++ bibtexdatabase/src/test/java/wad/ApplicationTest.java
@@ -1,8 +1,11 @@
+package wad;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 import org.fluentlenium.adapter.FluentTest;
 
 import org.junit.Test;
@@ -14,19 +17,16 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  *
  * @author santeri
  */
-public class NewArticleTest extends FluentTest {
-    
-public WebDriver webDriver = new HtmlUnitDriver();
+public class ApplicationTest extends FluentTest {
+    public WebDriver webDriver = new HtmlUnitDriver();
     public WebDriver getDefaultDriver(){
         return webDriver;
     }
     
     @Test
-    public void submitArticle(){
-        goTo("http://bibtexdatabase.herokuapp.com/articles/new");
-        fill("#author").with("Santeri");
-        fill("#title").with("Eeppinen kandi");
-        submit("input[type=submit]");
-        assertTrue(pageSource().contains("New article created"));
+    public void titleContainsBibtex() {
+        goTo("http://bibtexdatabase.herokuapp.com/");
+        assertTrue(title().contains("bibtexdatabase"));
     }
+    
 }
--- bibtexdatabase/src/test/java/wad/controller/ArticleControllerTest.java
+++ bibtexdatabase/src/test/java/wad/controller/ArticleControllerTest.java
@@ -1,9 +1,10 @@
+package wad.controller;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 import org.fluentlenium.adapter.FluentTest;
 
 import org.junit.Test;
@@ -15,16 +16,19 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver;
  *
  * @author santeri
  */
-public class applicationTest extends FluentTest {
-    public WebDriver webDriver = new HtmlUnitDriver();
+public class ArticleControllerTest extends FluentTest {
+    
+public WebDriver webDriver = new HtmlUnitDriver();
     public WebDriver getDefaultDriver(){
         return webDriver;
     }
     
     @Test
-    public void titleContainsBibtex() {
-        goTo("http://bibtexdatabase.herokuapp.com/");
-        assertTrue(title().contains("bibtexdatabase"));
+    public void submitArticle(){
+        goTo("http://bibtexdatabase.herokuapp.com/articles/new");
+        fill("#author").with("Santeri");
+        fill("#title").with("Eeppinen kandi");
+        submit("input[type=submit]");
+        assertTrue(pageSource().contains("New article created"));
     }
-    
 }
