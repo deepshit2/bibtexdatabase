27018ac0ef7d245c3c46d881513489180e474cb6
ued1 <ued1@users.noreply.github.com>
2015-10-02 18:50:28 +0300

    ArticleServiceTest

---
+++ bibtexdatabase/src/test/java/wad/service/ArticleServiceTest.java
@@ -0,0 +1,88 @@
+
+package wad.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import wad.Application;
+import wad.domain.Article;
+import wad.repository.ArticleRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class ArticleServiceTest {
+
+    @Autowired
+    private ArticleService articleService;
+    
+    @Autowired
+    private ArticleRepository articleRepository;
+    
+    private Article article1;
+    private Article article2;
+    
+    @Before
+    public void setUp() {
+        article1 = new Article();
+        article1.setAuthor("kirjoittaja1");
+        article1.setTitle("otsikko1");
+        article1.setJournal("journal");
+        article1.setYear(2001);
+        article1.setVolume(2);
+        article2 = new Article();
+        article2.setAuthor("kirjoittaja2");
+        article2.setTitle("otsikko2");
+        article2.setJournal("journal2");
+        article2.setYear(2002);
+        article2.setVolume(3);
+        
+    }
+    
+    @Test
+    public void testAddArticle() {
+        articleService.addArticle(article1);
+        Article retrieved = articleRepository.findOne(article1.getId());
+        assertEquals(retrieved.getId(), article1.getId());
+        assertEquals(retrieved.getAuthor(), article1.getAuthor());
+        assertEquals(retrieved.getTitle(), article1.getTitle());
+    }
+    
+    @Test
+    public void testDeleteArticle() {
+        articleRepository.save(article1);
+        Long id = article1.getId();
+        articleService.deleteArticle(article1.getId());
+        Article eiOle = articleRepository.findOne(id);
+        assertTrue(eiOle == null);
+    }
+    
+    @Test
+    public void testGetArticle() {
+        articleRepository.save(article1);
+        Article retrieved = articleService.getArticle(article1.getId());
+        assertEquals(retrieved.getId(), article1.getId());
+        assertEquals(retrieved.getAuthor(), article1.getAuthor());
+        assertEquals(retrieved.getTitle(), article1.getTitle());
+    }
+    
+    @Test
+    public void testListArticles() {
+        articleRepository.save(article1);
+        articleRepository.save(article2);
+        List<Article> articles = articleService.list();
+        List<String> titles = new ArrayList();
+        for(Article a : articles) {
+            titles.add(a.getTitle());
+        }
+        assertTrue(titles.contains(article1.getTitle()));
+        assertTrue(titles.contains(article2.getTitle()));
+    }
+    
+}
