4188ebe461944435fe8514637134604cd803d9a7
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2015-10-06 19:49:17 +0300

    Default kontrolleri palauttaa olioita kannasta

--- bibtexdatabase/src/main/java/wad/controller/DefaultController.java
+++ bibtexdatabase/src/main/java/wad/controller/DefaultController.java
@@ -1,13 +1,42 @@
 package wad.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import wad.service.ArticleService;
+import wad.service.BookService;
+import wad.service.InproceedingsService;
+import wad.service.MastersthesisService;
+import wad.service.TechreportService;
 
 @Controller
 public class DefaultController {
 
-    @RequestMapping("*")
-    public String index() {
+     
+    @Autowired
+    BookService bookService;
+    
+    @Autowired
+    ArticleService articleService;
+    
+    @Autowired
+    InproceedingsService inproceedingsService;
+    
+    @Autowired
+    MastersthesisService mastersthesisService;
+
+    @Autowired
+    TechreportService techreportService;
+    
+    @RequestMapping(value = "*", method = RequestMethod.GET)
+    public String listAll(Model model){
+        model.addAttribute("articles",articleService.list());
+        model.addAttribute("books",bookService.list());
+        model.addAttribute("inproceedings", inproceedingsService.list());
+        model.addAttribute("mastersthesis", mastersthesisService.list());
+        model.addAttribute("techreports", techreportService.list());
         return "index";
     }
 }
