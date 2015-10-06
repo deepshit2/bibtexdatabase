35ffc9b6d1dd4dcf3b7b410299676eee1df09775
Otto-Ville Raitolahti <raot@cs.helsinki.fi>
2015-10-06 20:09:03 +0300

    Lisätty hakutoiminnallisuus

--- bibtexdatabase/src/main/java/wad/controller/DefaultController.java
+++ bibtexdatabase/src/main/java/wad/controller/DefaultController.java
@@ -1,13 +1,17 @@
 package wad.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class DefaultController {
-
+    
+    
     @RequestMapping("*")
-    public String index() {
+    public String index(Model model) {
         return "index";
     }
 }
