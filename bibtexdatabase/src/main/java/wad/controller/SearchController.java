162ff804e45ac8b586a74c2c454a01f748deadd1
Otto-Ville Raitolahti <raot@cs.helsinki.fi>
2015-10-18 12:54:01 +0300

    haku tagin perusteella

--- bibtexdatabase/src/main/java/wad/controller/SearchController.java
+++ bibtexdatabase/src/main/java/wad/controller/SearchController.java
@@ -20,12 +20,22 @@ import wad.repository.MastersthesisRepository;
 import wad.repository.TechreportRepository;
 import wad.service.ArticleService;
 import wad.service.BookService;
+import wad.service.BookletService;
+import wad.service.ConferenceService;
+import wad.service.InbookService;
+import wad.service.IncollectionService;
 import wad.service.InproceedingsService;
+import wad.service.ManualService;
 import wad.service.MastersthesisService;
+import wad.service.MiscService;
+import wad.service.PhdthesisService;
+import wad.service.ProceedingsService;
+import wad.service.TagService;
 import wad.service.TechreportService;
+import wad.service.UnpublishedService;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping(value = "/search")
 public class SearchController {
 
     @Autowired
@@ -43,13 +53,76 @@ public class SearchController {
     @Autowired
     private TechreportService techreportService;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public String list(@RequestParam String search, Model model) {
-        model.addAttribute("articles", articleService.search(search));
+    @Autowired
+    private BookletService bookletService;
+
+    @Autowired
+    private ConferenceService conferenceService;
+
+    @Autowired
+    private InbookService inbookService;
+
+    @Autowired
+    private IncollectionService incollectionService;
+
+    @Autowired
+    private ManualService manualService;
+
+    @Autowired
+    private MiscService miscService;
+
+    @Autowired
+    private PhdthesisService phdthesisService;
+
+    @Autowired
+    private ProceedingsService proceedingsService;
+
+    @Autowired
+    private TagService tagService;
+
+    @Autowired
+    private UnpublishedService unpublishedService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String search() {
+        return "search";
+    }
+
+    @RequestMapping(value = "/keyword", method = RequestMethod.GET)
+    public String find(@RequestParam String search, Model model) {
         model.addAttribute("books", bookService.search(search));
+        model.addAttribute("articles", articleService.search(search));
         model.addAttribute("inproceedings", inproceedingsService.search(search));
         model.addAttribute("mastersthesises", mastersthesisService.search(search));
         model.addAttribute("techreports", techreportService.search(search));
+        model.addAttribute("booklets", bookletService.search(search));
+        model.addAttribute("conferences", conferenceService.search(search));
+        model.addAttribute("inbooks", inbookService.search(search));
+        model.addAttribute("incollections", incollectionService.search(search));
+        model.addAttribute("manuals", manualService.search(search));
+        model.addAttribute("miscs", miscService.search(search));
+        model.addAttribute("phdtheses", phdthesisService.search(search));
+        model.addAttribute("proceedings", proceedingsService.search(search));
+        model.addAttribute("unpublished", unpublishedService.search(search));
+        return "index";
+    }
+
+    @RequestMapping(value = "/tag", method = RequestMethod.GET)
+    public String findByTag(@RequestParam String tag, Model model) {
+        model.addAttribute("books", tagService.getBooks(tag));
+        model.addAttribute("articles", tagService.getArticles(tag));
+        model.addAttribute("inproceedings", tagService.getInproceedings(tag));
+        model.addAttribute("mastersthesises", tagService.getMastersthesises(tag));
+        model.addAttribute("techreports", tagService.getTechreports(tag));
+        model.addAttribute("booklets", tagService.getBooklets(tag));
+        model.addAttribute("conferences", tagService.getConferences(tag));
+        model.addAttribute("inbooks", tagService.getInbooks(tag));
+        model.addAttribute("incollections", tagService.getIncollections(tag));
+        model.addAttribute("manuals", tagService.getManuals(tag));
+        model.addAttribute("miscs", tagService.getMiscs(tag));
+        model.addAttribute("phdtheses", tagService.getPhdthesises(tag));
+        model.addAttribute("proceedings", tagService.getProceedings(tag));
+        model.addAttribute("unpublished", tagService.getUnpublished(tag));
         return "index";
     }
 }
